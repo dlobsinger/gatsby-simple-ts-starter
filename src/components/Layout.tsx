397568e12a639188eb3c07a397dae5ba8a479fbd
Dave Lobsinger <dave.lobsinger@wishabi.com>
2018-11-06 15:23:58 -0500

    Move css

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -4,7 +4,7 @@ import Helmet from 'react-helmet'
 import { StaticQuery, graphql } from 'gatsby'
 
 import Header from './Header'
-import './layout.css'
+import '../css/layout.css'
 
 interface ILayoutProps {
   children?: ReactNode
